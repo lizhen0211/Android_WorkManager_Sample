ab8a68b7d71ed2df864c35a14d489714e35cd3b7
lizhen <lizhen@zhumengyuan.com>
2018-09-30 15:06:57 +0800

    baseline

---
+++ app/src/main/java/com/lz/workmanager/example/CompressWorker.java
@@ -0,0 +1,40 @@
+package com.lz.workmanager.example;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+/**
+ * Created by lz on 2018/9/30.
+ */
+public class CompressWorker extends Worker {
+
+    public CompressWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters params) {
+        super(context, params);
+    }
+
+    @Override
+    public Worker.Result doWork() {
+
+        // Do the work here--in this case, compress the stored images.
+        // In this example no parameters are passed; the task is
+        // assumed to be "compress the whole library."
+        myCompress();
+
+        // Indicate success or failure with your return value:
+        return Result.SUCCESS;
+
+        // (Returning RETRY tells WorkManager to try this task again
+        // later; FAILURE says not to try again.)
+    }
+
+    private void myCompress() {
+        Log.e("workmanager", "compressing");
+    }
+}
+
---
+++ app/src/main/java/com/lz/workmanager/example/MyCacheCleanupWorker.java
@@ -0,0 +1,19 @@
+package com.lz.workmanager.example;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import androidx.work.Worker;
+
+/**
+ * Created by lz on 2018/9/30.
+ */
+public class MyCacheCleanupWorker extends Worker {
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Log.e("workmanager", "Cleanup MyCache");
+        return Result.SUCCESS;
+    }
+}
